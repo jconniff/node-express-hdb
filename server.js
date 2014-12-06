4fab84b93fa0890fe32eb1996223f0e6b8f352eb
Jeff Conniff <jeffconniff@yahoo.com>
2014-12-05 18:06:32 -0800

    done with exercize

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require('express');
+var exphbs = require('express3-handlebars');
 var app = express();
+
+
+// from https://github.com/ericf/express-handlebars  in the "app.js" section
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // app.get(...) is called "a route"
 // req = "a request"
 // res = "a response"
 app.get('/', function(req, res){
-	res.send('hello, world');
+	var randNumber = Math.round(Math.random() * 10);
+	//res.send('hello, world');
+	
+	// now with the handlebars stuff from ericf, do res.render(...) instead of res.send(...)
+	//res.render('index');
+
+	// now pass a variable to the renderer
+	res.render('index', {
+		randNumber: randNumber
+	});
+
+});
+
+// add another route
+app.get('/about', function(req, res){
+	res.render('about');
 });
 
 // this "serves files in the public directory"
